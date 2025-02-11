e963545775cb12ec236120e8705d435bc5fadc16
Hyun_W <69100145+yehang218@users.noreply.github.com>
2025-02-12 02:18:37 +0900

    :sparkles: 컴퓨터 점수 및 플레이어 점수 구현

--- hotfist/src/main/java/com/seok/hotfist/aggregate/MasterScore.java
+++ hotfist/src/main/java/com/seok/hotfist/aggregate/MasterScore.java
@@ -2,19 +2,25 @@ package com.seok.hotfist.aggregate;
 
 // 고수들의 송판 격파 점수
 public enum MasterScore {
-    FIRST(10),
-    SECOND(20),
-    THIRD(30),
-    FOURTH(40),
-    FIFTH(50);
+    STAGE_1("초보 격파왕 '강수지'", 10),
+    STAGE_2("중급 격파왕 '고윤석'", 20),
+    STAGE_3("숙련 격파왕 ‘곽우석'", 30),
+    STAGE_4("고수 격파왕 ‘김성민'",40),
+    STAGE_5("전설 격파왕 ‘불주먹 최혜민’", 50);
 
-    private final int SCORE;
+    private final String MASTERNAME;
+    private final int POWER;
 
-    MasterScore(int score) {
-        SCORE = score;
+    MasterScore(String masterName, int power) {
+        MASTERNAME = masterName;
+        POWER = power;
     }
 
-    public int getSCORE() {
-        return SCORE;
+    public int getPOWER() {
+        return POWER;
+    }
+
+    public String getMASTERNAME() {
+        return MASTERNAME;
     }
 }
