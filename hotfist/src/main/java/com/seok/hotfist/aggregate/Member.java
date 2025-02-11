bf6005ec73be960db6baedfa4e775d401b798588
jakego1 <jakego13129@gmail.com>
2025-02-11 21:41:43 +0900

    MemberService, MemberRepository 기본 설정

--- hotfist/src/main/java/com/seok/hotfist/aggregate/Member.java
+++ hotfist/src/main/java/com/seok/hotfist/aggregate/Member.java
@@ -1,4 +1,73 @@
 package com.seok.hotfist.aggregate;
 
-public class Member {
+import java.io.Serializable;
+
+public class Member implements Serializable {
+    private int memberNo;
+    private String id;
+    private String pwd;
+    private String nickname;
+    private int score;
+
+    public Member() {
+    }
+
+    public Member(int memberNo, String id, String pwd, String nickname, int score) {
+        this.memberNo = memberNo;
+        this.id = id;
+        this.pwd = pwd;
+        this.nickname = nickname;
+        this.score = score;
+    }
+
+    public int getMemberNo() {
+        return memberNo;
+    }
+
+    public void setMemberNo(int memberNo) {
+        this.memberNo = memberNo;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getPwd() {
+        return pwd;
+    }
+
+    public void setPwd(String pwd) {
+        this.pwd = pwd;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    @Override
+    public String toString() {
+        return "Member{" +
+                "memberNo=" + memberNo +
+                ", id='" + id + '\'' +
+                ", pwd='" + pwd + '\'' +
+                ", nickname='" + nickname + '\'' +
+                ", score=" + score +
+                '}';
+    }
 }
--- hotfist/src/main/java/com/seok/hotfist/run/Application.java
+++ hotfist/src/main/java/com/seok/hotfist/run/Application.java
@@ -1,8 +1,12 @@
 package com.seok.hotfist.run;
 
+import com.seok.hotfist.service.MemberService;
+
 import java.util.Scanner;
 
 public class Application {
+
+    private static final MemberService ms = new MemberService();
 
     public static void main(String[] args) {
 
@@ -18,8 +22,7 @@ public class Application {
             switch (menuChoice) {
                 case 1: break;
                 case 2: break;
-                case 3: break;
-                case 4:
+                case 3:
                     System.out.println("당신의 주먹은 0점이군요. 게임을 종료합니다."); return;
                 default:
                     System.out.println("번호를 잘 누르지 못하는 사람은 주먹이 약할 확률이 높다 \n - 무슨무슨 서양 격언 -");
