4f735b37bd300e7f8d789a832e660d1ff7080d61
jakego1 <jakego13129@gmail.com>
2025-02-12 17:00:30 +0900

    [RANKING] / #16 / 동일 점수 동일 등수 표기 기능 & 오류 수정

--- hotfist/src/main/java/com/seok/hotfist/repository/RankRepository.java
+++ hotfist/src/main/java/com/seok/hotfist/repository/RankRepository.java
@@ -13,16 +13,11 @@ public class RankRepository {
     private final File file = new File("src/main/java/com/seok/hotfist/db/rankDB.dat");
 
     public RankRepository() {
-        if (!file.exists()) {
-            initialData();
-        }
         loadRanks();
     }
 
-
     public void saveRank(Member member) {
         rankList.add(member);
-
         Collections.sort(rankList, new RankComparator());
 
         if (rankList.size() > 10) {
@@ -43,36 +38,35 @@ public class RankRepository {
 
     private void loadRanks() {
         if (!file.exists()) {
-            System.out.println("랭크가 존재하지 않습니다.");
+            System.out.println("초기 랭크 데이터를 생성합니다.");
+            createInitialData();
             return;
         }
 
         try (ObjectInputStream ois = new ObjectInputStream(
                 new BufferedInputStream(new FileInputStream(file)))) {
-
-            rankList.addAll((List<Member>) ois.readObject());
-
+            List<Member> loadedList = (List<Member>) ois.readObject();
+            rankList.clear();
+            rankList.addAll(loadedList);
         } catch (IOException | ClassNotFoundException e) {
-            throw new RuntimeException(e);
+            System.out.println("랭크 데이터 로드 중 오류가 발생했습니다. 초기 데이터를 생성합니다.");
+            createInitialData();
         }
     }
 
-    private void initialData() {
+    private void createInitialData() {
+        rankList.clear();
 
-        if (!file.exists()) {
-            rankList.add(new Member(1, "user01", "pass01", "리신", 300));
-            rankList.add(new Member(2, "user02", "pass02", "엘리스", 224));
-            rankList.add(new Member(3, "user03", "pass03", "카타리나", 150));
-            rankList.add(new Member(4, "user04", "pass04", "멜", 280));
-            rankList.add(new Member(5, "user05", "pass05", "오로라", 260));
-            rankList.add(new Member(6, "user06", "pass06", "요네", 198));
-            rankList.add(new Member(7, "user07", "pass07", "뽀삐", 170));
-            rankList.add(new Member(8, "user08", "pass08", "제이스", 185));
-            rankList.add(new Member(9, "user09", "pass09", "징크스", 190));
-            rankList.add(new Member(10, "user10", "pass10", "케인", 177));
-
-        }
-
+        rankList.add(new Member(1, "user01", "pass01", "리신", 300));
+        rankList.add(new Member(2, "user02", "pass02", "엘리스", 224));
+        rankList.add(new Member(3, "user03", "pass03", "카타리나", 150));
+        rankList.add(new Member(4, "user04", "pass04", "멜", 280));
+        rankList.add(new Member(5, "user05", "pass05", "오로라", 260));
+        rankList.add(new Member(6, "user06", "pass06", "요네", 198));
+        rankList.add(new Member(7, "user07", "pass07", "뽀삐", 170));
+        rankList.add(new Member(8, "user08", "pass08", "제이스", 185));
+        rankList.add(new Member(9, "user09", "pass09", "징크스", 190));
+        rankList.add(new Member(10, "user10", "pass10", "케인", 177));
 
         Collections.sort(rankList, new RankComparator());
 
@@ -80,7 +74,7 @@ public class RankRepository {
                 new BufferedOutputStream(new FileOutputStream(file)))) {
             oos.writeObject(rankList);
         } catch (IOException e) {
-            throw new RuntimeException(e);
+            throw new RuntimeException("초기 데이터 저장 중 오류가 발생했습니다.", e);
         }
     }
 
--- hotfist/src/main/java/com/seok/hotfist/service/RankService.java
+++ hotfist/src/main/java/com/seok/hotfist/service/RankService.java
@@ -9,7 +9,7 @@ public class RankService {
     private final RankRepository rankRepository = new RankRepository();
 
     public void registerRank(int memberId, String nickname, int score) {
-        Member member = new Member();
+        Member member = new Member(memberId, "temp_id", "temp_pass", nickname, score);
         rankRepository.saveRank(member);
         System.out.println("신기록을 달성하여 " + nickname + "님의 점수 " + score + " 점이 랭킹에 등록되었습니다.");
     }
@@ -17,11 +17,28 @@ public class RankService {
     public void displayRanks() {
         List<Member> ranks = rankRepository.getRanks();
         System.out.println("===== 불주먹 랭킹 목록 =====");
+
+        if (ranks.isEmpty()) {
+            System.out.println("등록된 랭킹이 없습니다.");
+            return;
+        }
+
+        int currentRank = 1;
+        int sameRankCount = 0;
+        int previousScore = ranks.get(0).getScore();
+
         for (int i = 0; i < ranks.size(); i++) {
             Member member = ranks.get(i);
-            System.out.println((i + 1) + "위 | " + member.getNickname() + " | " + member.getScore() + "점");
+
+            if (i > 0 && previousScore != member.getScore()) {
+                currentRank += sameRankCount;
+                sameRankCount = 0;
+            }
+
+            System.out.println(currentRank + "위 | " + member.getNickname() + " | " + member.getScore() + "점");
+
+            previousScore = member.getScore();
+            sameRankCount++;
         }
     }
-
-
 }
