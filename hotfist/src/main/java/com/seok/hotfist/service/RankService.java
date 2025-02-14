ec2b6c070ac1fea76009c8d8c27bcc8d21843e48
jakego1 <jakego13129@gmail.com>
2025-02-14 20:08:28 +0900

    로비에서 게임 랭크 화면 표시 기능

--- hotfist/src/main/java/com/seok/hotfist/service/RankService.java
+++ hotfist/src/main/java/com/seok/hotfist/service/RankService.java
@@ -44,31 +44,31 @@ public class RankService {
         return topRanksList;
     }
 
-    public void displayRanks() {
-        List<Member> ranks = rankRepository.getRanks();
-        System.out.println("===== 불주먹 랭킹 목록 =====");
-
-        if (ranks.isEmpty()) {
-            System.out.println("등록된 랭킹이 없습니다.");
-            return;
-        }
-
-        int currentRank = 1;
-        int sameRankCount = 0;
-//        int previousScore = ranks.get(0).getScore();
-
-        for (int i = 0; i < ranks.size(); i++) {
-            Member member = ranks.get(i);
-
-//            if (i > 0 && previousScore != member.getScore()) {
-//                currentRank += sameRankCount;
-//                sameRankCount = 0;
-//            }
+//    public void displayRanks() {
+//        List<Member> ranks = rankRepository.getRanks();
+//        System.out.println("===== 불주먹 랭킹 목록 =====");
 //
-//            System.out.println(currentRank + "위 | " + member.getNickname() + " | " + member.getScore() + "점");
+//        if (ranks.isEmpty()) {
+//            System.out.println("등록된 랭킹이 없습니다.");
+//            return;
+//        }
 //
-//            previousScore = member.getScore();
-//            sameRankCount++;
-        }
-    }
+//        int currentRank = 1;
+//        int sameRankCount = 0;
+////        int previousScore = ranks.get(0).getScore();
+//
+//        for (int i = 0; i < ranks.size(); i++) {
+//            Member member = ranks.get(i);
+//
+////            if (i > 0 && previousScore != member.getScore()) {
+////                currentRank += sameRankCount;
+////                sameRankCount = 0;
+////            }
+////
+////            System.out.println(currentRank + "위 | " + member.getNickname() + " | " + member.getScore() + "점");
+////
+////            previousScore = member.getScore();
+////            sameRankCount++;
+//        }
+//    }
 }
